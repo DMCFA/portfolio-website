3030c53b16c07319dec99cb7589bba11a1e97a2b
Duarte Almeida <duartemalmeida@gmail.com>
2021-02-26 11:03:44 +0000

    finalized the nav bar on the css and js file with small adjustaments

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const NavSlide = () => {
             if (link.style.animation) {
                 link.style.animation = ''
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
             }
         });
+        burger.classList.toggle('toggle');
     });
 }
 
