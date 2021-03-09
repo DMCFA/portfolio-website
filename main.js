a92acb796e5c5e9fb8471494f46d808feba00b05
Duarte Almeida <duartemalmeida@gmail.com>
2021-03-09 10:52:49 +0000

    fixed the nav bar which wasnt closing  after clicked

--- main.js
+++ main.js
@@ -2,27 +2,38 @@
 Mobile Nav bar
 */
 
-function NavSlide () {
+const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-
-    burger.addEventListener('click', () => {
-
-        nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
+  
+    navLinks.forEach((link, index) => {
+      link.addEventListener('click', () => {
         burger.classList.toggle('toggle');
+        nav.classList.toggle('nav-active');
+        navLinks.forEach(setStyleLink)
+      })
     });
-}
+  
+    burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+      // animate navlinks
+      navLinks.forEach(setStyleLink);
+      // burger animation
+      burger.classList.toggle('toggle');
+    });
+  }
+  
+  function setStyleLink(el, index) {
+    if (el.style.animation) {
+      el.style.animation = ''
+    } else {
+      el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+    }
+  }
+  navSlide();
 
-NavSlide()
 
 /*
 Window Animation
