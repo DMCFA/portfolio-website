bb83ebee254de6cb78f56ac36f0ad587e4d2c69b
Duarte Almeida <duartemalmeida@gmail.com>
2021-06-30 23:58:59 +0100

    fix some small bugs around the opening/closing burger nav

--- main.js
+++ main.js
@@ -3,43 +3,42 @@ Mobile Nav bar
 */
 
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-  
-    navLinks.forEach((link, index) => {
-      link.addEventListener('click', () => {
-        burger.classList.toggle('toggle');
-        nav.classList.toggle('nav-active');
-        navLinks.forEach(setStyleLink)
-      })
-    });
-  
-    burger.addEventListener('click', () => {
-      // Toggle Nav
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  navLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+      burger.classList.toggle('toggle');
       nav.classList.toggle('nav-active');
-      // animate navlinks
-      navLinks.forEach(setStyleLink);
-      // burger animation
-      // burger.classList.toggle('toggle');
-    });
-  }
-  
-  function setStyleLink(el, index) {
-    if (el.style.animation) {
-      el.style.animation = ''
-    } else {
-      el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
-    }
-  }
+      navLinks.forEach(setStyleLink)
+    })
+  });
+
+  burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+    // animate navlinks
+    navLinks.forEach(setStyleLink);
+    // burger animation
+    burger.classList.toggle('toggle');
+  });
+}
+
+function setStyleLink(el, index) {
+  if (el.style.animation) {
+    el.style.animation = ''
+  } else {
+    el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+  }}
   navSlide();
-
-
-/*
-Window Animation
-*/
-
-window.addEventListener('load', () => {
+  
+  
+  /*
+  Window Animation
+  */
+  
+  window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
       easing: 'ease-in-out',
@@ -47,20 +46,20 @@ window.addEventListener('load', () => {
       mirror: false
     })
   });
-
-
+  
+  
   /*
   ButtoTn Animation
   */
-
+  
   document.addEventListener("scroll", handleScroll);
   // get a reference to our predefined button
   var scrollToTopBtn = document.querySelector(".top-btn");
-  
+
   function handleScroll() {
     var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var GOLDEN_RATIO = 0.1;
-  
+
     if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
       //show button
       scrollToTopBtn.style.display = "block";
@@ -69,7 +68,7 @@ window.addEventListener('load', () => {
       scrollToTopBtn.style.display = "none";
     }
   }
-  
+
   scrollToTopBtn.addEventListener("click", scrollToTop);
   
   function scrollToTop() {
@@ -77,4 +76,4 @@ window.addEventListener('load', () => {
       top: 0,
       behavior: "smooth"
     });
-  }
+  } 
